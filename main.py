82648792285c14d6f21bd0a58670e9a0fb0d8671
LorincziMatyas <lorinczi.matyas@student.ms.sapientia.ro>
2024-03-23 00:50:16 +0200

    Changed the project backend to python flask

---
+++ main.py
@@ -0,0 +1,26 @@
+from flask import Flask, jsonify
+from flask_cors import CORS
+from threading import Thread
+import time
+
+app = Flask(__name__)
+CORS(app)
+
+
+def start_logic():
+    while True:
+        print("logic started")
+        time.sleep(10)
+
+
+@app.route('/api/data')
+def get_data():
+    print('YEEES')
+    data = {'message': 'Hello from Python backend!'}
+    return jsonify(data)
+
+
+if __name__ == '__main__':
+    thread = Thread(target=start_logic)
+    thread.start()
+    app.run(debug=True)
